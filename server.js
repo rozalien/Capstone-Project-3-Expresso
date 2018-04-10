bc94ffb07bce5fbad00e39e2be61c81e0d6cf977
Heather L. Cribbs <30633279+rozalien@users.noreply.github.com>
2018-04-10 09:07:58 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,25 +1,26 @@
-
 require('./migration.js');
 
 const bodyParser = require('body-parser');
-//const cors = require('cors');
-//const errorhandler = require('errorhandler');
+
+const cors = require('cors');
+
 const express = require('express');
+const app = express();
+
+app.use(cors());
 
 const apiRouter = require('./api/api');
 
-const app = express();
+
 const PORT = process.env.PORT || 4000;
 
 app.use(bodyParser.json());
-//app.use(cors());
 
 app.use('/api', apiRouter);
-
-//app.use(errorhandler());
 
 app.listen(PORT, () => {
   console.log('Listening on port: ' + PORT);
 });
 
 module.exports = app;
+
